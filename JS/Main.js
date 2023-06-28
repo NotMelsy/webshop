1b0f281221d53a49038987d02cf91085f66aff91
Melle Siegers <mellesiegers15@gmail.com>
2023-06-28 11:45:04 +0200

    product overzicht

--- JS/Main.js
+++ JS/Main.js
@@ -26,5 +26,72 @@ function showSlides(n) {
   dots[slideIndex-1].className += " active";
 }
 
+// SHOES
+let allGames = document.getElementsByClassName("game");
+let filters = document.getElementsByClassName("filter");
 
 
+setInterval(function(){
+    document.getElementsByTagName("body")[0].style.overflow = "auto";
+}, 1500);//laat scrollen na 1.5s
+
+window.scrollTo(0, 0);//zet window naar boven bij refresh
+
+for(let i = 0; i < filters.length; i++){
+    filters[i].checked = true;
+}//zet checkboxes op checked bij refresh
+
+let Vicinity = document.getElementById("checkbox-Vicinity");
+let Nike = document.getElementById("checkbox-Nike");
+let NewB = document.getElementById("checkbox-NewB");
+
+Vicinity.onchange = function(){
+    if(Vicinity.checked === true){
+        for(let i = 0; i < allGames.length; i++){
+            if(allGames[i].dataset.category === "Vicinity"){    
+                allGames[i].style.display = "block";
+            }
+        }
+    }//laat list items zien
+    else{
+        for(let i = 0; i < allGames.length; i++){
+            if(allGames[i].dataset.category === "Vicinity"){    
+                allGames[i].style.display = "none";
+            }
+        }
+    }//laat list items verdwijnen
+}
+
+Nike.onchange = function(){
+    if(Nike.checked === true){
+        for(let i = 0; i < allGames.length; i++){
+            if(allGames[i].dataset.category === "Nike"){    
+                allGames[i].style.display = "block";
+            }
+        }
+    }//laat list items zien
+    else{
+        for(let i = 0; i < allGames.length; i++){
+            if(allGames[i].dataset.category === "Nike"){    
+                allGames[i].style.display = "none";
+            }
+        }
+    }//laat list items verdwijnen
+}
+
+NewB.onchange = function(){
+    if(NewB.checked === true){
+        for(let i = 0; i < allGames.length; i++){
+            if(allGames[i].dataset.category === "NewB"){    
+                allGames[i].style.display = "block";
+            }
+        }
+    }//laat list items zien
+    else{
+        for(let i = 0; i < allGames.length; i++){
+            if(allGames[i].dataset.category === "NewB"){    
+                allGames[i].style.display = "none";
+            }
+        }
+    }//laat list items verdwijnen
+}
